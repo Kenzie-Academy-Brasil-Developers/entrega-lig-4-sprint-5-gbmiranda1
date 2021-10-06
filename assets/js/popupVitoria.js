3dd1416dc90477347312411d52058356924af24b
Lucas Andrade de Lara <lscynz1995@gmail.com>
2021-10-06 14:41:08 -0300

    Optimizando a estilizacao do popup

--- assets/js/popupVitoria.js
+++ assets/js/popupVitoria.js
@@ -1,6 +1,7 @@
 function mostrarPopUp(mostrarTela, texto) {
     const popup = document.getElementById(mostrarTela)
     const h3 = document.getElementById("texto-vitoria")
+    h3.classList.add("texto-da-vitoria")
     h3.innerText = texto
     popup.classList.add("visivel")
     popup.addEventListener("click", (e) => {
@@ -11,4 +12,4 @@ function mostrarPopUp(mostrarTela, texto) {
 }
 
 // TESTE PARA O POPUP, SO DESCOMENTAR
-//mostrarPopUp("telaVitoria")
+mostrarPopUp("telaVitoria")
